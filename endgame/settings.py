50f0a94a2ef7ed3ec6bc9fa395fa70fe7ffb2f91
erichilario <reacherichilario@gmail.com>
2019-04-04 14:40:07 +0930

    task 1 done but not finalised

--- endgame/settings.py
+++ endgame/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hp%y=$2njmx2acx@me6i3@s4jys*s55nz48pin+ep-49-t%pl!'
+# SECRET_KEY = 'hp%y=$2njmx2acx@me6i3@s4jys*s55nz48pin+ep-49-t%pl!'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'hp%y=$2njmx2acx@me6i3@s4jys*s55nz48pin+ep-49-t%pl!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
